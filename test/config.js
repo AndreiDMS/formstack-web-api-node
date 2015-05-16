d66dcb2db0e7dcf93d121a47f44a5c146506ca34
Andrei Domsa <andrei@sybsoftware.com>
2015-05-16 23:05:31 +0300

    update test config file

--- test/config.js
+++ test/config.js
@@ -1,1 +1,22 @@
-global.ACCESS_TOKEN = 'REPLACE-WITH-FS-ACCESS-TOKEN';
+global.fsaConf = {
+	ACCESS_TOKEN: 'xxxxx',	// Valid FS App Access Token
+	FORM_DETAILS_ID: '0000', // Valid form id
+	FORM_DETAILS_NAME: 'xxxx',	// Form name for FORM_DETAILS_ID form
+	FORM_SUBMIT_ID: '0000',	// Valid form id
+	submitFormFieldIds: [	// Valid form fields ids
+	     0000,	// Name
+	     0000,	// Email
+	     0000,	// Short Answer
+	     0000	// Long Answer
+	],
+	submitFormFieldValues: [
+	    {
+	    	first: 'FirstN',
+	    	last: 'LastN'
+	    },
+	    'xxxx@xxxx.xxx',
+	    'Short Answer',
+	    'Long Answer'
+	],
+	EDIT_SUBMISSION_ID: '0000' // Valid submission id
+}
